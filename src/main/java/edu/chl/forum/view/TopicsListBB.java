a7f1371105f280e021bcd39afa63c7396b70405d
eriher <eriher@ed3358-01.ed3358.studat.rh6.ii.2.hb.s.cdal.chalmers.se>
2014-10-23 11:41:49 +0200

    database working

--- src/main/java/edu/chl/forum/view/TopicsListBB.java
+++ src/main/java/edu/chl/forum/view/TopicsListBB.java
@@ -6,8 +6,6 @@
 
 package edu.chl.forum.view;
 
-import edu.chl.forum.core.Forum;
-import edu.chl.forum.core.ForumThread;
 import edu.chl.forum.core.IForum;
 import edu.chl.forum.core.MainTopic;
 import edu.chl.forum.core.SubTopic;
@@ -16,30 +14,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.component.accordionpanel.AccordionPanel;
 
 /**
  *
  * @author erik
  */
-@ManagedBean
-@ViewScoped
+@Named
+@SessionScoped
 public class TopicsListBB implements Serializable{
         private static final Logger LOG = Logger.getLogger(TopicsListBB.class.getName()); 
         private List<MainTopic> maintopics;
         private List<SubTopic> subtopics;
         private List<String> strings;
         @Inject IForum forum;
-        
+        private String tabs;        
         
         @PostConstruct
         public void init() {
             
             LOG.log(Level.INFO, "TopicsListBB alive {0}", this);
-                    /*subtopics = new ArrayList();
+                    subtopics = new ArrayList();
                     subtopics.add(new SubTopic("Subtopic 1","context",null));
                     subtopics.add(new SubTopic("Subtopic 2","context",null));
                     forum.getMainTopicCatalogue().create(new MainTopic("Test1",subtopics));
@@ -56,13 +55,29 @@ public class TopicsListBB implements Serializable{
                     subtopics.add(new SubTopic("Subtopic 4","context",null));
                     subtopics.add(new SubTopic("Subtopic 5","context",null));
                     subtopics.add(new SubTopic("Subtopic 6","context",null));
-                    forum.getMainTopicCatalogue().create(new MainTopic("Test3",subtopics));*/
+                    forum.getMainTopicCatalogue().create(new MainTopic("Test3",subtopics));
                     maintopics = forum.getMainTopicCatalogue().findAll();
+                    setActiveTabs(forum.getMainTopicCatalogue().count());
+                    System.out.println(tabs);
             System.out.println(forum.getMainTopicCatalogue().count());
             //subtopics = maintopics.get(0).getList();
         }
         public List<MainTopic> getmaintopics() {
             return maintopics;
+        }
+        public String gettabs() {
+            return tabs;
+        }
+
+        
+        private void setActiveTabs(int count) {
+            tabs = new String();
+            for(int i = 0;i<count;i++){
+                if(i==0)
+                tabs = Integer.toString(i);
+                else
+                tabs = tabs + "," + Integer.toString(i);
+            }       
         }
     
 }
